b1d95e0cbecc754d6d30c3255a135d766b1070c6
Jakob <63636667+jakobfrederikson@users.noreply.github.com>
2021-09-22 11:44:04 +1200

    Add files via upload

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{   
+    class Program
+    {
+        static void print(string arg)
+        {
+            Console.WriteLine($"{arg}, this is how you call a method in C#.\n");
+        }
+
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string world = "World!";
+            Console.WriteLine($"{hello} {world} My name is Jakob!");
+            Console.WriteLine($"The variable {hello} has {hello.Length} letters.");
+            Console.WriteLine($"The variable {world} has {world.Length} letters.");
+
+            if (hello.Length <= 5)
+            {
+                Console.WriteLine("\nThis is in an if statement!");
+                Console.WriteLine("Pretty similar to C++, awesome :) \n");
+            }
+
+            print("Wow");
+        }
+    } 
+}
